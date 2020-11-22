58c0e6fb7d425eb52172ecc3dafc40f7f30d44c1
qiangxuejian <984686036@qq.com>
2020-11-22 15:59:53 +0800

    用指针打印数组

---
+++ 打印数组/打印数组/test.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int *p = arr;
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+	return 0;
+}
